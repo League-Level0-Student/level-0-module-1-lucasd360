fd414a65b7be10daad87821ce1b3c694830f6cc9
lucasd360 <35010273+lucasd360@users.noreply.github.com>
2018-02-13 17:02:12 -0800

    Add files via upload

--- src/RobotInSpace.java
+++ src/RobotInSpace.java
@@ -7,6 +7,7 @@
 	import javax.sound.sampled.AudioInputStream; 
 	import javax.sound.sampled.AudioSystem; 
 	import javax.sound.sampled.Clip;
+import javax.swing.JOptionPane;
 
 import org.jointheleague.graphical.robot.Robot;
 
@@ -15,6 +16,7 @@ import org.jointheleague.graphical.robot.Robot;
 		
 	Robot rob = new Robot("mini");
 	
+	
 	/*  
 	  Make the Robot move around the screen when the arrow keys are pressed... 
 	  
@@ -22,17 +24,34 @@ import org.jointheleague.graphical.robot.Robot;
 	  
 	 */
 
-	private void moveRobot(int keyPressed) {
+	private void moveRobot(int keyPressed) throws InterruptedException {
+		
 	    // 2. Print out the keyPressed variable and write down the numbers for each arrow key
 
 	    // 3. If the up arrow is pressed, move the Robot up the screen.
+		System.out.println(keyPressed);
+		if(keyPressed==38) {
+			rob.setAngle(0);
+			rob.microMove(5);
 
+			
+		}
 	    // 4. If the down arrow is pressed, move the Robot down.
+		if(keyPressed==40) {
+			rob.setAngle(180);
+			rob.microMove(5);
+		}
 
 	    // 5. If the left arrow is pressed, make the Robot go left.
-	   
+		if(keyPressed==39) {
+			rob.setAngle(90);
+			rob.microMove(5);
+		}
 	    // 6. If right is pressed, move the Robot right.
-	    
+		if(keyPressed==37) {
+			rob.setAngle(-90);
+			rob.microMove(5);
+		}
 	    // 7. Run your program and move the Robot to RD-2D for a surprise! 
 	}
 
@@ -58,7 +77,12 @@ import org.jointheleague.graphical.robot.Robot;
 
 	public boolean dispatchKeyEvent(KeyEvent e) {
 	    if (e.getID() == KeyEvent.KEY_PRESSED) {
-	        moveRobot(e.getKeyCode());
+	        try {
+				moveRobot(e.getKeyCode());
+			} catch (InterruptedException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
 	        try {
 	            checkIfR2D2Found();
 	        } catch (Exception exception) {
--- src/TheRiddler.java
+++ src/TheRiddler.java
@@ -1,3 +1,5 @@
+import javax.swing.JOptionPane;
+
 //    Copyright (c) The League of Amazing Programmers 2013-2017
 //    Level 0
 
@@ -8,9 +10,15 @@ public class TheRiddler {
 	public static void main(String[] args) {
 
 		// 1. Make a variable to hold the score
+int intscore = 0;
 
 		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
-
+String answer = JOptionPane.showInputDialog("What is brown and sticky?");
+if(answer.equals("A stick")){
+	 JOptionPane.showMessageDialog(null, "Correct");
+	
+	 
+}
 		// 4. If they got the answer right, pop up "correct!" and increase the score by one
 
 		// 5. Otherwise, say "wrong" and tell them the answer
